f9209cc287603082d7732d42e79a5d3ae467a36e
Sunny Patel <sunnypatel4prez@gmail.com>
2024-08-16 12:58:44 -0500

    first commit

---
+++ database/config.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
+)
+
+// DB is a global variable to hold the database connection
+var DB *pgx.Conn
+
+// Connect initializes the database connection
+func Connect() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Connect to PostgreSQL
+	connStr := os.Getenv("DATABASE_URL")
+	var err error
+	DB, err = pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
+	})
+
+	app.Listen(":" + PORT)
+}
